96515cc74ea1c3401391267c654f83bf619d2aab
lolsokje <alex9521@hotmail.com>
2019-10-10 15:40:21 +0200

    Added Role command for role toggling

---
+++ src/commands/general/Role.js
@@ -0,0 +1,36 @@
+const patron = require('patron.js');
+const Constants = require('../../utility/Constants');
+
+class Role extends patron.Command {
+    constructor() {
+        super({
+            names: ['role'],
+            groupName: 'allpeoplehere',
+            description: 'Adds or removes a role from a user',
+            args: [
+                new patron.Argument({
+                    name: 'role',
+                    key: 'role',
+                    type: 'string',
+                    example: 'Ferrari'
+                })
+            ]
+        });
+    }
+
+    async run(msg, args) {
+        const existingRole = msg.guild.roles.find(r => r.name === args.role);
+
+        if (!existingRole) {
+            return msg.sender.reply(`Role ${args.role} doesn't exist`, { color: Constants.standardColors.red });
+        } else if (msg.member.roles.find(r => r.name === args.role)) {
+            await msg.member.roles.remove(existingRole);
+            return msg.sender.reply(`Role ${args.role} removed from ${msg.member.displayName}`, { color: existingRole.color });
+        } else {
+            await msg.member.roles.add(existingRole);
+            return msg.sender.reply(`Role ${args.role} added to ${msg.member.displayName}`, { color: existingRole.color });
+        }
+    }
+}
+
+module.exports = new Role();
