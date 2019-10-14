a40b0feebf0cb1f4e17c8a30d072b16f5186ae62
lolsokje <alex9521@hotmail.com>
2019-10-14 08:23:45 +0200

    Send message to #general on member verification

--- src/commands/administration/Verify.js
+++ src/commands/administration/Verify.js
@@ -15,14 +15,15 @@ class Verify extends patron.Command {
         const member = msg.mentions.members.first();
         const welcomeRole = msg.guild.roles.find(r => r.name === 'Welcome');
         const allpeoplehereRole = msg.guild.roles.find(r => r.name === 'allpeoplehere');
+        const generalChannel = msg.guild.channels.find(c => c.name === 'general');
 
         await member.roles.remove(welcomeRole);
         await member.roles.add(allpeoplehereRole);
 
         const message = await msg.sender.reply(`${member.displayName} verified`, { color: Constants.standardColors.green });
-        msg.delete(3000);
-        message.delete(3000);
-        return message;
+        await msg.delete(3000);
+        await message.delete(3000);
+        return generalChannel.send(`**Welcome ${member.displayName}!**`);
     }
 }
 
